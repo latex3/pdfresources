0c5aa35f750964d4fb066dab432494c1e0c250c5
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-03-11 23:09:15 +0100

    more simplification

--- config-dvips.lua
+++ config-dvips.lua
@@ -1,6 +1,4 @@
 testfiledir  = "testfiles-dvips"
 checkengines = {"latexdvips"}
 stdengine     = "latexdvips"
-
-checkruns=2
 ps2pdfopt = " -sDocumentUUID=DocumentUUID -sInstanceUUID=InstanceUUID -dCompressStreams=false -dCompressPages=false -dNOSAFER "
