4f6ce351f7d7b5318522ca3f86b47fe4ecf4fe40
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-03-16 22:37:08 +0100

    adapt tests to new l3build, new dvipdfmx

--- config-dvips.lua
+++ config-dvips.lua
@@ -1,4 +1,5 @@
 testfiledir  = "testfiles-dvips"
 checkengines = {"latexdvips"}
 stdengine     = "latexdvips"
-ps2pdfopt = " -sDocumentUUID=DocumentUUID -sInstanceUUID=InstanceUUID -dCompressStreams=false -dCompressPages=false -dNOSAFER "
+-- ps2pdfopt = " -sDocumentUUID=DocumentUUID -sInstanceUUID=InstanceUUID -dCompressStreams=false -dCompressPages=false -dNOSAFER "
+ps2pdfopt = " -dNOSAFER "
