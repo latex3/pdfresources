f17f551264ef792e907064516d9b1c990d82f1fd
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-12-30 20:23:41 +0100

    remade more tests after intarray change

--- resave.bat
+++ resave.bat
@@ -4,3 +4,16 @@ rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 
 REM leave (id)  - ./build/test/viewerpreferences.xetex.pdf.fc
+
+l3build save -cconfig-noxetex -eluatex calculationsortorder
+l3build save -cconfig-noxetex -epdftex calculationsortorder
+l3build save -cconfig-noxetex -eluatex extgstate-patches
+l3build save -cconfig-noxetex -epdftex extgstate-patches
+l3build save -cconfig-noxetex -eluatex metadata
+l3build save -cconfig-noxetex -epdftex metadata
+l3build save -cconfig-noxetex -eluatex pgfpatches
+l3build save -cconfig-noxetex -epdftex pgfpatches
+l3build save -cconfig-noxetex -eluatex test-new-transparent
+l3build save -cconfig-noxetex -epdftex test-new-transparent
+l3build save -cconfig-noxetex -eluatex transparent-tikz
+l3build save -cconfig-noxetex -epdftex transparent-tikz
