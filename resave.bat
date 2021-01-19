e3c74ec8788a8fee1646f8cb14a21be236bc67d5
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-01-19 23:48:35 +0100

    remade tests, after color changes

--- resave.bat
+++ resave.bat
@@ -2,3 +2,9 @@ rem */\(1+[a-z\-]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
+
+l3build save -eluatex -cconfig-noxetex pgfpatches
+l3build save -epdftex -cconfig-noxetex pgfpatches
+l3build save -cconfig-luatex non-ascii-links
+
+l3build save -cconfig-dvips destination-options
