3c7a53cbdfdb3ec9735c6321755f2492b24296bc
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-01-20 19:01:30 +0100

    open tests

--- resave.bat
+++ resave.bat
@@ -1,9 +1,27 @@
-rem */\(1+[a-z\-]\)\.\(2+[a-z]\)\.*
+rem */\(1+[a-z\-\_]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
+rem l3build save -cconfig-noxetex -e\2 \1
 
-l3build save -cconfig-noxetex -eluatex calculationsortorder
-l3build save -cconfig-noxetex -epdftex calculationsortorder
-l3build save -cconfig-noxetex -eluatex metadata
-l3build save -cconfig-noxetex -epdftex metadata
+REM Failed tests for configuration config-noxetex:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-noxetex/calculationsortorder.luatex.pdf.fc
+REM   - ./build/test-config-noxetex/calculationsortorder.pdftex.pdf.fc
+REM   - ./build/test-config-noxetex/metadata.luatex.pdf.fc
+REM   - ./build/test-config-noxetex/metadata.pdftex.pdf.fc
+REM   - ./build/test-config-noxetex/pgfpatches.luatex.pdf.fc
+REM   - ./build/test-config-noxetex/pgfpatches.pdftex.pdf.fc
+REM
+REM Failed tests for configuration config-luatex:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-luatex/non-ascii-links.luatex.pdf.fc
+REM
+REM Failed tests for configuration config-dvips:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-dvips/catalogAF.latexdvips.pdf.fc
+REM   - ./build/test-config-dvips/embedfile.latexdvips.pdf.fc
+REM   - ./build/test-config-dvips/ismap.latexdvips.pdf.f
