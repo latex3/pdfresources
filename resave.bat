733ce33866ca6034329f61af3117e4e97f49c5cd
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-01-22 00:28:49 +0100

    remade ocgcolorlinks tests

--- resave.bat
+++ resave.bat
@@ -3,3 +3,7 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 rem l3build save -cconfig-noxetex -e\2 \1
+
+l3build save -eluatex ocgcolorlinks
+l3build save -epdftex ocgcolorlinks
+l3build save -exetex ocgcolorlinks
