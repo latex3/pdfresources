48f0ba8496fdec8fcac29de863924165c5259981
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-01-27 16:17:41 +0100

    redo tests (more spaces after code change)

--- resave.bat
+++ resave.bat
@@ -7,3 +7,21 @@ rem l3build save -cconfig-noxetex -e\2 \1
 REM l3build save -eluatex ocgcolorlinks
 REM l3build save -epdftex ocgcolorlinks
 REM l3build save -exetex ocgcolorlinks
+
+REM   - ./build/test-config-noxetex/metadata.luatex.pdf.fc
+REM   - ./build/test-config-noxetex/metadata.pdftex.pdf.fc
+
+REM
+REM Failed tests for configuration config-noxetex:
+REM
+REM   Check failed with difference files
+REM
+REM Failed tests for configuration config-luatex:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-luatex/non-ascii-links.luatex.pdf.fc
+REM
+REM Failed tests for configuration config-dvips:
+REM
+REM   Check failed with difference files
+REM   - ./build/test-config-dvips/pdffilelink.latexdvips.pdf.fc
