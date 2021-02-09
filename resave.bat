b3014bdc561232d2f3b224378faa416440743db9
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-02-09 10:37:27 +0100

    more regression code into style for now

--- resave.bat
+++ resave.bat
@@ -6,3 +6,7 @@ rem l3build save -cconfig-noxetex -e\2 \1
 
 REM   - ./build/test-config-noxetex/metadata.luatex.pdf.fc
 REM   - ./build/test-config-noxetex/metadata.pdftex.pdf.fc
+
+l3build save -eluatex annot
+l3build save -epdftex annot
+l3build save -exetex annot
