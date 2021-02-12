a3632ef9bdd0eed6bce324bffa97407a68b2619b
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-02-12 16:40:54 +0100

    new regression-test

--- resave.bat
+++ resave.bat
@@ -3,3 +3,30 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 rem l3build save -cconfig-noxetex -e\2 \1
+
+
+
+REM testfiles/mixpageattr.pvt
+REM testfiles/ocgcolorlinks.pvt
+REM testfiles/ocproperties.pvt
+REM testfiles/pageattr.pvt
+REM testfiles/pagelabels-log.lvt
+REM testfiles/pagelabels.pvt
+REM testfiles/pagesattr.pvt
+REM testfiles/pdffilelink.pvt
+REM testfiles/pdf_pageobject_ref.lvt
+REM testfiles/show.lvt
+REM testfiles/text-convert.lvt
+REM testfiles/thispageattr.pvt
+REM testfiles/version-compare.lvt
+REM testfiles/viewerpreferences.pvt
+REM testfiles/xform-preamble.pvt
+REM testfiles/xform.pvt
+REM testfiles-dvips/annot.pvt
+REM testfiles-dvips/bdc-properties.pvt
+REM testfiles-dvips/catalogAF.pvt
+REM testfiles-dvips/destination-options.pvt
+REM testfiles-dvips/ismap.pvt
+REM testfiles-dvips/linkhook.pvt
+REM testfiles-dvips/pagelabels.pvt
+REM testfiles-dvips/pdffilelink.pvt
