3b8b9ac02046ad79d8ffbab7261c702a7e674402
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-02-14 00:10:16 +0100

    make pdfdict_name internal

--- resave.bat
+++ resave.bat
@@ -3,13 +3,3 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 rem l3build save -cconfig-noxetex -e\2 \1
-
-l3build save -cconfig-dvips annot-last
-l3build save -cconfig-dvips catalogAF
-l3build save -cconfig-dvips destination-options
-l3build save -cconfig-dvips dict_if_empty
-l3build save -cconfig-dvips pagelabels-log
-l3build save -cconfig-dvips pdf_pageobject_ref
-l3build save -cconfig-dvips show
-l3build save -cconfig-dvips text-convert
-l3build save -cconfig-dvips version-compare
