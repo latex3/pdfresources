90bc6121b864a2ef15378f8f4a642099e3a7707d
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-02-21 22:48:49 +0100

    prepare ctan

--- build.lua
+++ build.lua
@@ -1,26 +1,36 @@
 #!/usr/bin/env texlua
 
--- Build script for "pdfresources" bundle
+-- Build script for "LaTeX PDF management testphase bundle" bundle
 
 -- Identify the bundle and module
 bundle  = ""
-module  = "pdfresources"
+module  = "pdfmanagement-testphase"
 
 
 installfiles = {"*.sty","*.cls","*.def","*.lua","*.ltx"}
-sourcefiles =  {"*.dtx","*.ins",
+
+if options["target"] == "ctan" then
+  sourcefiles =  {"*.dtx","*.ins",
                 "l3ref-tmp.sty",
-                "pdfmanagement-regressiontest.sty",
-                "hgeneric-testphase.def",
                 "firstaid/transparent-ltx.sty",          
                 "firstaid/hyperxmp-patches-tmp-ltx.sty",                
                 "firstaid/pdflscape-ltx.sty",
                 "firstaid/xcolor-patches-tmp-ltx.sty",                
+                 }
+else
+ sourcefiles =  {"*.dtx","*.ins",
+                "l3ref-tmp.sty",
+                "firstaid/transparent-ltx.sty",          
+                "firstaid/hyperxmp-patches-tmp-ltx.sty",                
+                "firstaid/pdflscape-ltx.sty",
+                "firstaid/xcolor-patches-tmp-ltx.sty",  
+                "support/pdfmanagement-regressiontest.sty",              
                 "newpackages/new-bookmark.sty",
                 "newpackages/bkm-generic.def",
                 "newpackages/new-attachfile.sty",
                 "newpackages/atfi-generic.def"                
                 }
+end 
 
 checkruns = 3
 
