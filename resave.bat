edc05473a63c89041b127158f4a9de4e4e88da2b
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-03-03 12:18:45 +0100

    adapt tests to tl2021, corrections in opacity

--- resave.bat
+++ resave.bat
@@ -1,4 +1,4 @@
-rem */\(1+[a-zA-Z\-\_]\)\.\(2+[a-z]\)\.*
+rem */\(1+[a-zA-Z0-9\-\_]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
