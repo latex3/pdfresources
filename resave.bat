09704a0d63056ae3f2815f563538441e6e350cfe
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-03-17 17:57:39 +0100

    storing

--- resave.bat
+++ resave.bat
@@ -3,21 +3,3 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 rem l3build save -cconfig-noxetex -e\2 \1
-
-Rem Check failed with difference files
-l3build save -eluatex disable-links
-l3build save -epdftex disable-links
-l3build save -exetex disable-links
-l3build save -eluatex linkbordercolor
-l3build save -epdftex linkbordercolor
-l3build save -exetex linkbordercolor
-l3build save -eluatex ocgcolorlinks
-l3build save -epdftex ocgcolorlinks
-l3build save -exetex ocgcolorlinks
-l3build save -eluatex pdffilelink
-l3build save -epdftex pdffilelink
-l3build save -exetex pdffilelink
-
-Rem  Check failed with difference files
-l3build save -cconfig-dvips disable-links
-l3build save -cconfig-dvips pdffilelink
