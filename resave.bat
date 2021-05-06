ea61347da9e4cd9239282b1bb3462e5f65aa74a8
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-05-06 20:30:10 +0200

    remade xetex tests

--- resave.bat
+++ resave.bat
@@ -3,12 +3,3 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 rem l3build save -cconfig-noxetex -e\2 \1
-
-
-l3build save -exetex checkbox
-l3build save -exetex disable-links
-l3build save -exetex linkbordercolor
-l3build save -exetex linkhook
-l3build save -exetex linkmargin
-l3build save -exetex ocgcolorlinks
-l3build save -exetex pgfpatches
