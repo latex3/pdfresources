7ad86a6e6ac43c1d0c6fe37e9aa2d61cde8c95ee
Ulrike Fischer <fischer@troubleshooting-tex.de>
2022-09-09 18:40:57 +0200

    update more tests

--- resave.bat
+++ resave.bat
@@ -3,3 +3,15 @@ rem l3build save -e\2 \1
 rem l3build save -cconfig-dvips \1
 rem l3build save -cconfig-luatex -e\2 \1
 rem l3build save -cconfig-noxetex -e\2 \1
+
+l3build save -eluatex,pdftex,xetex annot
+l3build save -eluatex,pdftex,xetex annot-dict
+l3build save -eluatex,pdftex,xetex annot-last
+l3build save -eluatex,pdftex,xetex ap-names
+l3build save -eluatex,pdftex,xetex bdc-properties
+l3build save -eluatex,pdftex,xetex calculate-textfield
+l3build save -eluatex,pdftex,xetex catalogAF
+l3build save -eluatex,pdftex,xetex checkbox
+l3build save -eluatex,pdftex,xetex colorprofile-standard
+l3build save -eluatex,pdftex,xetex colorprofile1
+l3build save -eluatex,pdftex,xetex destination-options
